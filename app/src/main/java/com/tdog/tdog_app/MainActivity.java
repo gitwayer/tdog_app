0616392380ab540278c2b9a071ce9a8f939defd1
gitwayer <948516534@qq.com>
2019-06-14 22:18:34 +0800

    测试提交

--- app/src/main/java/com/tdog/tdog_app/MainActivity.java
+++ app/src/main/java/com/tdog/tdog_app/MainActivity.java
@@ -2,6 +2,7 @@ package com.tdog.tdog_app;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("tag","sss");
     }
 }
